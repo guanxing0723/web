c58ad3bce83a7fd579d533c40d683208f4e8a222
guanxing0723 <aa900723@gmail.com>
2020-06-28 23:38:49 +0800

    first commit

---
+++ d10813038/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $("#yt").click(function (e) { 
+        location.assign("https://www.youtube.com/watch?v=V3aU9hWv4Ic");
+    });
+    $("#net").click(function (e) { 
+        location.assign("https://www.youtube.com/watch?v=yEIRc9BZwlU");
+    });
+});
---
+++ youtubeplayer/playlist.js
@@ -0,0 +1,10 @@
+//YouTube影片ID
+let playList = [
+    "otJ-c6nMZyQ",//like it is
+    "3K_h-T74nxs",//attack音D
+];
+//播放起訖秒數
+let playTime = [
+    [0,178],
+    [0,282]
+];
